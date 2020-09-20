1496a9e8462a04ddd94cb001d37444b95b21eb0b
arthur.barbero <arthur-barbero@hotmail.com>
2020-09-20 11:49:13 -0300

    fix: Model errors

--- back-end/src/model/account.py
+++ back-end/src/model/account.py
@@ -2,16 +2,16 @@ from datetime import datetime
 
 from mongoengine import Document, EmailField, StringField, BooleanField, DateTimeField, ReferenceField, IntField
 
-from src.model.utils.validations.Validator import not_empty, string_validator
+from src.model.utils.validations import Validator
 from src.model.address import Address
 
 class Account(Document):
 
-    nome = StringField(max_length=100, validation=[not_empty, string_validator])
-    sobrenome = StringField(max_length=100)
+    name = StringField(max_length=100, required=True, validation=Validator.not_empty)
+    last_name = StringField(max_length=100, required=True)
     email = EmailField(required=True, unique=True)
-    senha = StringField(max_length=100)
-    dt_nasc = DateTimeField()
-    endereço = ReferenceField(Address)
+    password = StringField(max_length=100, required=True)
+    birth_dt = DateTimeField(required=True)
+    address = ReferenceField(Address, required=True)
     created_at = DateTimeField(default=datetime.utcnow)
     updated_at = DateTimeField(default=datetime.utcnow)
