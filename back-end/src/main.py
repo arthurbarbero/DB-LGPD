2764f6bb5a89b016261cf561464a28c4220e3a94
arthur.barbero <arthur-barbero@hotmail.com>
2020-09-22 18:10:03 -0300

    feat little hello

--- back-end/src/main.py
+++ back-end/src/main.py
@@ -9,6 +9,10 @@ load_dotenv(find_dotenv())
 
 app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return 'Hello'
+
 app.wsgi_app = DBConnect(app.wsgi_app)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
