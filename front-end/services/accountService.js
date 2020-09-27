6b0a42f3da40a5a83b8ba175cdf2f223d1df0032
arthur.barbero <arthur-barbero@hotmail.com>
2020-09-27 15:19:21 -0300

    fix:

--- front-end/services/accountService.js
+++ front-end/services/accountService.js
@@ -3,7 +3,7 @@ const BASE_URL = 'http://127.0.0.1:5000';
 
 const register = async obj => {
     try {
-      const res = await axios.post(`${BASE_URL}/account/register`, {data: obj});
+      const res = await axios.post(`${BASE_URL}/account/register`, { data: obj });
       return res.data;
   
     } catch (e) {
--- front-end/utils/cript.js
+++ front-end/utils/cript.js
@@ -1,10 +1,9 @@
-const key = 'ThyagoOdoricoAmaLinux'
+const key = 'U2FsdGVkX1+h7dM5y1SLpVq/hDcf5ekhjrQDONSkHF50iLTMrL/sRQuxKDQp0sLFSGeG/i+kJACeFcWwDFX1QhDu0swataPmbDAgO2nozmikZNAmFR159xFakr0BfX/wPpHODJGkmYALhITwOS3My/f6qXlCD08v9VKE70SbZrX2uxq8YhAm1EGEluFHBO7bXXuSUvVMn3UtH4QziDGe9CC6smq8hJcl4Hsz0w0mVqZ8S11B9HwPdDxbqpWlpALQMbNrrlA3Qa8bfLTtell+xB3f5EtQ66RgTFY+68tP2FCfQ5NVCiaq6DAFcRpN7cVZwoMwY9nmhBSRvLnujB8kyaA81s7coOu+NXIRBHI3k4PSKgrx+JbkLuCszW7saruR'
 
 function encrypt(text) {
     return CryptoJS.AES.encrypt(text, key).toString()
 }
 
 function decrypt(text) {
-    var bytes  = CryptoJS.AES.decrypt(text, key);
-    return bytes.toString(CryptoJS.enc.Utf8);
+    return CryptoJS.AES.decrypt(text, key).toString(CryptoJS.enc.Utf8);
 }
