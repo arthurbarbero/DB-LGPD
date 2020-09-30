e3446dbf8ea92f4d50cedcaba2123c8b200f3d1e
arthur.barbero <arthur-barbero@hotmail.com>
2020-09-30 18:29:54 -0300

    Feat: signup-page update

--- front-end/javaScript/signup.js
+++ front-end/javaScript/signup.js
@@ -1,5 +1,5 @@
 const BASE_URL = 'http://127.0.0.1:5000'
-const idInputs = ['nome', 'sobrenome', 'email', 'senha', 'ddd', 'cel', 'mesnasc', 'dianasc', 'anonasc', 'cpf', 'rua', 'bairro', 'cep', 'numeros', 'estado', 'cidade', 'complemento'];
+const idInputs = ['nome', 'sobrenome', 'email', 'senha', 'ddd', 'cel', 'nasc', 'cpf', 'rua', 'bairro', 'cep', 'numeros', 'estado', 'cidade', 'complemento'];
 
 let cliente = new Object();
 
@@ -17,6 +17,22 @@ function getInputsById() {
 }
 function saveData() {
     getInputsById();
-    cliente = encrypt(JSON.stringify(cliente));
+    cliente = encrypt(JSON.stringify(
+        {
+            first_name: cliente.nome,
+            last_name: cliente.sobrenome,
+            email: cliente.email,
+            password: cliente.senha,
+            birth_dt: cliente.nasc,
+            address : {
+                street: cliente.rua,
+                neighbourhood: cliente.bairro,
+                number: cliente.numeros,
+                zip_code: cliente.cep,
+                city: cliente.cidade,
+                state: cliente.estado,
+            }
+        }
+    ));
     sendData(cliente);
 }
