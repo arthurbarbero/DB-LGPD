d328def4a137deb1666397044a4462ceaff812f4
arthur.barbero <arthur-barbero@hotmail.com>
2020-10-18 15:16:24 -0300

    Feat: Login functions

--- front-end/javaScript/login.js
+++ front-end/javaScript/login.js
@@ -1,6 +1,5 @@
 window.onload = () => {
-    const idUser = localStorage.setItem('user_id', response.data.id);
-    if(idUser) {
+    if ( localStorage.getItem('user_id') ) {
         window.location.href = 'homepage.html';
     }
 }
@@ -8,14 +7,16 @@ window.onload = () => {
 async function login() {
     const email = document.querySelector('#email');
     const password = document.querySelector('#password');
-    const data = encrypt(JSON.stringify({'email': email, 'password': password}));
+    const data = encrypt(JSON.stringify({'email': email.value, 'password': password.value}));
     
-    await axios.post('http://127.0.0.1:5000/account/login', data)
+    await axios.post('http://127.0.0.1:5000/account/login', {'data': data}, {headers: { 'Access-Control-Allow-Origin':'*', 'Content-Type': 'application/json' }})
     .then(response => {
+        console.log(response.data)
         localStorage.setItem('user_id', response.data.id);
         window.location.href = 'homepage.html';
     })
     .catch(response => { 
+        console.log(response)
         setToasted(false, 'Failed to do sign-in');
     })
 }
