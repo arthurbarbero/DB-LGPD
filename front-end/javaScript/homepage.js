39ce45d2604b81c578225ffbd933868dd4634c78
arthur.barbero <arthur-barbero@hotmail.com>
2020-10-17 23:09:19 -0300

    Fixed header buttons

--- front-end/javaScript/homepage.js
+++ front-end/javaScript/homepage.js
@@ -1,12 +1,39 @@
 window.onload = () => {
     if (localStorage.getItem('user_id')) {
-        navMenu = document.getElementsByClassName('nav-menu')[0].children
+        let navMenu = document.getElementById('nav-menu')
+        
+        let signup = document.getElementById('btn-cadastre-se')
+        let login = document.getElementById('btn-login')
 
-        navMenu[0].innerText = 'Meu perfil'
-        navMenu[1].innerText = 'Logout'
+        navMenu.removeChild(signup)
+        navMenu.removeChild(login)
+        
+        let meuPerfil = document.createElement('button')
+        meuPerfil.setAttribute("id", "btn-login" )
+        meuPerfil.setAttribute("type", "input" )
+        meuPerfil.setAttribute("onclick", "window.location.href = 'profile_page.html'" )
 
-        navMenu[0].setAttribute('onclick', "window.location.href='profile_page.html'")
-        navMenu[1].setAttribute('onclick', "logout()")
+        let img = document.createElement('img')
+        img.setAttribute("class", "icon-header")
+        img.setAttribute("id", "icon-perfil")
+        img.setAttribute("src", "/front-end/icons/perfil.svg")
+        img.setAttribute("alt", "perfil")
+
+        let text = document.createElement('span')
+        text.innerText = "Meu Perfil"
+
+        meuPerfil.appendChild(img)
+        meuPerfil.appendChild(text)
+
+        navMenu.appendChild(meuPerfil)
+
+        let logout = document.createElement('button')
+        logout.setAttribute('id','btn-cadastre-se')
+        logout.setAttribute('type','input')
+        logout.setAttribute('onclick','logout()')
+        logout.innerText = 'Logout'
+
+        navMenu.appendChild(logout)
     }
 }
 
